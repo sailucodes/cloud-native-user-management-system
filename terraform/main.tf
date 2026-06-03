109793de9a46d09f185b8bb9c4b721ecb2f71c23
Chavala Sailaja Sridevi <sailajasridevich@gmail.com>
2026-06-03 14:08:22 +0530

    Add Terraform infrastructure configuration

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "cloud-native-user-management-system-bucket"
+}
+
+resource "aws_dynamodb_table" "users" {
+  name         = "Users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+}
